f021df968b10d08868a2b1fd0e27a5471c341973
sam coianiz <samuel.coianiz@oneplacesolutions.com>
2022-08-31 14:29:51 +1000

    End result

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyBZRRTS4p8KnBiLkxIuPOqoO3n3P3ZpLw8",
+      authDomain: "buy-and-sell-82ba5.firebaseapp.com",
+      projectId: "buy-and-sell-82ba5",
+      storageBucket: "buy-and-sell-82ba5.appspot.com",
+      messagingSenderId: "1036453100697",
+      appId: "1:1036453100697:web:5a37228bc1e8af56768c08",
+      measurementId: "G-CGE8KWF2B7"
+    }
 };
 
 /*
